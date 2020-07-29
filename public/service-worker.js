355bc910bf8b92376212120894ecf3698c33ad49
CheaC <cheacliatt@gmail.com>
2020-07-29 16:56:54 -0400

    Added some error catches to functions

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,45 +1,72 @@
-const FILES_TO_CACHE = ["/", "index.html", "index.js", "favicon.ico", "db.js", "manifest.webmanifest", "service-worker.js", "styles.css", "/icons/icon-192x192.png", "/icons/icon-512x512.png" ];
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "db.js",
+  "favicon.ico",
+  "index.js",
+  "manifest.webmanifest",
+  "service-worker.js",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+  "/styles.css"
+];
 
-const CACHE_NAME = "static-cache-v2";
+const CACHE_NAME = "static-cache-v1";
 const DATA_CACHE_NAME = "data-cache-v1";
 
 // install
-self.addEventListener("install", function(evt) {
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
+self.addEventListener("install", function (evt) {
+evt.waitUntil(
+  caches
+    .open(CACHE_NAME)
+    .then((cache) => {
       console.log("Your files were pre-cached successfully!");
-      return cache.addAll(FILES_TO_CACHE);
+      cache
+        .addAll(FILES_TO_CACHE)
+        .then((result) => {
+          // debugger;
+          console.log("result of add all", result);
+        })
+        .catch((err) => {
+          // debugger;
+          console.log("Add all error: ", err);
+        });
     })
-  );
+    .catch((err) => {
+      console.log(err);
+    })
+);
 
-  self.skipWaiting();
+self.skipWaiting();
 });
 
 // activate
-self.addEventListener("activate", function(evt) {
-  evt.waitUntil(
-    caches.keys().then(keyList => {
-      return Promise.all(
-        keyList.map(key => {
-          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-            console.log("Removing old cache data", key);
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
+self.addEventListener("activate", function (evt) {
+evt.waitUntil(
+  caches.keys().then((keyList) => {
+    return Promise.all(
+      keyList.map((key) => {
+        if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+          console.log("Removing old cache data", key);
+          return caches.delete(key);
+        }
+      })
+    );
+  })
+);
 
-  self.clients.claim();
+self.clients.claim();
 });
 
 // fetch
-self.addEventListener("fetch", function(evt) {
-  if (evt.request.url.includes("/api/")) {
-    evt.respondWith(
-      caches.open(DATA_CACHE_NAME).then(cache => {
+self.addEventListener("fetch", function (evt) {
+if (evt.request.url.includes("/api/")) {
+  evt.respondWith(
+    caches
+      .open(DATA_CACHE_NAME)
+      .then((cache) => {
         return fetch(evt.request)
-          .then(response => {
+          .then((response) => {
             // If the response was good, clone it and store it in the cache.
             if (response.status === 200) {
               cache.put(evt.request.url, response.clone());
@@ -47,12 +74,22 @@ self.addEventListener("fetch", function(evt) {
 
             return response;
           })
-          .catch(err => {
+          .catch((err) => {
             // Network request failed, try to get it from the cache.
             return cache.match(evt.request);
           });
-      }).catch(err => console.log(err))
-    );
+      })
+      .catch((err) => console.log(err))
+  );
 
-    return;
-}});
+  return;
+}
+
+evt.respondWith(
+  caches.open(CACHE_NAME).then((cache) => {
+    return cache.match(evt.request).then((response) => {
+      return response || fetch(evt.request);
+    });
+  })
+);
+});
